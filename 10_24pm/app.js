7dbae1c6d9bf65d6fa923d15cfa9aa53f95e19cb
skywalker-hunter <cs13b1008@iith.ac.in>
2017-05-02 03:49:25 +0530

    Final commit

---
+++ 10_24pm/app.js
@@ -0,0 +1,497 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var path    = require('path');
+var sessions = require('client-sessions');
+var fs = require('fs')
+var unzip = require('unzip')
+var mkdir = require('mkdirp')
+var extract = require('extract-zip')
+var AdmZip = require('adm-zip');
+var PythonShell = require('python-shell');
+const fileUpload = require('express-fileupload');
+var formidable = require('formidable');
+var sleep = require('sleep');
+var fse = require('fs-extra');
+var fsq = require('fs-sync');
+var exec = require('child_process').exec;
+var varstring = require("varstring");
+var S = require('string');
+var multer  = require('multer') //filename
+var sleep = require('sleep');
+var dir = './uploaded_files/';
+var port = process.env.PORT || 3001;
+
+var app = express();
+
+//app.use(fileUpload());
+
+app.use(express.static(__dirname + '/public'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+
+app.use(sessions({
+  cookieName: 'session',
+  secret: 'rgebjiuVihB5U7owfghij2ihBIUB334I765UHIojkbB',
+  duration: 30 * 60 * 1000,
+  activeDuration: 5 * 60 * 1000,
+}));
+
+
+var user_database = mongoose.createConnection('mongodb://localhost/test',function (err) {
+  if(err){
+    return console.log(err);
+  }else {
+    return console.log('successfully connected to mongodb');
+  }
+});
+
+var admin_database = mongoose.createConnection('mongodb://localhost/test1',function (err) {
+  if(err){
+    return console.log(err);
+  }else {
+    return console.log('successfully connected to mongodb');
+  }
+});
+
+var user_mySchema =mongoose.Schema({
+  username: {type: String, unique: true},
+  password: {type: String},
+  firstname: String,
+  lastname: String
+});
+
+var admin_mySchema =mongoose.Schema({
+  username: {type: String, unique: true},
+  password: {type: String}
+});
+
+var User = user_database.model('users',user_mySchema);
+
+var Admin = admin_database.model('users1',admin_mySchema);
+
+app.get('/', function (req, res) {
+if(!req.session.admin){   
+  if(req.session && req.session.user){
+    User.findOne({username: req.session.user.username},function(err,user){
+      if(!user){
+        req.session.reset();
+        res.sendFile(path.join(__dirname+'/public/login.html'));
+      }else{
+        res.locals.user = user;
+        res.redirect('/dashboard');
+      }
+    });
+  }else{
+  //  res.send(req.session.user);
+    res.sendFile(path.join(__dirname+'/public/login.html'));
+  }
+  }else{
+  res.redirect('/admin_dashboard');
+  }
+
+});
+
+
+app.get('/login',function (req,res,next) {
+if(!req.session.admin){		
+  if(req.session && req.session.user){
+    User.findOne({username: req.session.user.username},function(err,user){
+      if(!user){
+        req.session.reset();
+        res.sendFile(path.join(__dirname+'/public/login.html'));
+      }else{
+        res.locals.user = user;
+        res.redirect('/dashboard');
+      }
+    });
+  }else{
+    res.sendFile(path.join(__dirname+'/public/login.html'));
+  }
+  }else{
+	res.redirect('/admin_dashboard');
+	}
+});
+
+
+app.get('/admin_login',function (req,res,next) {
+if(!req.session.user){
+	if(req.session && req.session.admin){
+    	Admin.findOne({username: req.session.admin.username},function(err,admin){
+    		if(!admin){
+    	        req.session.reset();
+        		res.sendFile(path.join(__dirname+'/public/login.html'));
+    		}else{
+    			res.locals.admin = admin;
+        		res.redirect('/admin_dashboard');
+    		}
+    	});
+    }else{
+    	res.sendFile(path.join(__dirname+'/public/admin_login.html'));
+    }
+}else{
+	res.redirect('/admin_dashboard');
+}   		
+});
+
+
+app.get('/deleting_user',function (req,res,next) {
+if(!req.session.user){
+	if(req.session && req.session.admin){
+    	Admin.findOne({username: req.session.admin.username},function(err,admin){
+    		if(!admin){
+    	        req.session.reset();
+        		res.sendFile(path.join(__dirname+'/public/login.html'));
+    		}else{
+    			res.locals.admin = admin;
+        		res.sendFile(path.join(__dirname+'/public/remove_account.html'));
+    		}
+    	});
+    }else{
+    	res.sendFile(path.join(__dirname+'/public/admin_login.html'));
+    }
+}else{
+	res.redirect('/admin_dashboard');
+}  
+  //res.send(__dirname);
+  
+  //res.sendFile('/Users/killer/Desktop/last_login_and_signup/public/remove_account.html');
+});
+
+
+app.get('/forgot_password',function (req,res,next) {
+if(!req.session.admin){		
+  if(req.session && req.session.user){
+    User.findOne({username: req.session.user.username},function(err,user){
+      if(!user){
+        req.session.reset();
+        res.sendFile(path.join(__dirname+'/public/forgot_password.html'));
+      }else{
+        res.locals.user = user;
+        res.redirect('/dashboard');
+      }
+    });
+  }else{
+    res.sendFile(path.join(__dirname+'/public/forgot_password.html'));
+  }
+}else{
+	res.redirect('/dashboard');
+}
+});
+
+
+
+app.get('/signup',function (req,res,next) {
+
+if(!req.session.admin){	
+	//res.send(req.session.user);
+  if(req.session && req.session.user){
+    User.findOne({username: req.session.user.username},function(err,user){
+      if(!user){
+        req.session.reset();
+        res.sendFile(path.join(__dirname+'/public/signup.html'));
+      }else{
+        res.locals.user = user;
+        res.redirect('/dashboard');
+      }
+    });
+  }else{
+    res.sendFile(path.join(__dirname+'/public/signup.html'));
+  }
+}else{
+	res.redirect('/admin_dashboard');
+}
+});
+
+
+app.get('/logout',function(req,res){
+  req.session.reset();
+  res.redirect('/login');
+});
+
+app.get('/admin_dashboard',function (req,res) {
+
+  if(req.session && req.session.admin){
+    Admin.findOne({username: req.session.admin.username},function(err,admin){
+      if(!admin){
+        req.session.reset();
+        res.redirect('/admin_login');
+      }else{
+        res.locals.admin = admin;
+        res.sendFile(path.join(__dirname+'/public/admin_dashboard.html'));
+      }
+    });
+  }else{
+    res.redirect('/login');
+  }
+});
+
+
+app.get('/dashboard',function (req,res) {
+
+	if(!req.session.admin){
+		if(req.session && req.session.user){
+    		User.findOne({username: req.session.user.username},function(err,user){
+      		if(!user){
+        		req.session.reset();
+      	  		res.redirect('/login');
+      		}else{
+        		res.locals.user = user;
+        		res.sendFile(path.join(__dirname+'/public/dashboard.html'));
+      		}
+    	   });
+  	    }else{
+    	  res.redirect('/login');
+  	    }
+	}else{
+		res.redirect('/admin_dashboard');
+	}
+});
+
+app.post('/dashboard', function (req, res){
+  if(!req.session.admin){
+    if(req.session && req.session.user){
+        User.findOne({username: req.session.user.username},function(err,user){
+          if(!user){
+            req.session.reset();
+              res.redirect('/login');
+          }else{  
+            if (!fs.existsSync(dir+req.session.user.username)){
+              fs.mkdirSync(dir+req.session.user.username);
+            }
+            var form = new formidable.IncomingForm();
+        	
+
+            form.parse(req);
+        	  var filePath;
+
+            if (!fs.existsSync(dir)){
+              fs.mkdirSync(dir);
+            }
+
+            form.on('fileBegin', function (name, file){
+        		
+                file.path = __dirname + '/uploaded_files/'+req.session.user.username+'/'+ file.name;
+
+        		
+        		console.log('file path '+ file.path);
+            });
+
+
+            form.on('file', function (name, file){
+        		    var zipfile_path = (__dirname+'/uploaded_files/'+req.session.user.username+'/'+file.name).toString();
+        		    fs.createReadStream(zipfile_path).pipe(unzip.Extract({ path:__dirname+'/uploaded_files/'+req.session.user.username }));
+                console.log('Uploaded and unzipped' + file.name);
+                var stri = 
+                fs.writeFile(__dirname + '/uploaded_files/'+req.session.user.username+'/'+req.session.user.username +'.txt', file.name);
+            });
+            
+
+        	  function function1(){
+              fs.readFile(__dirname + '/uploaded_files/'+req.session.user.username+'/'+req.session.user.username +'.txt', 'utf8', function (err,data) {
+                    if (err) {
+                      return console.log(err);
+                    }
+                    console.log(data);
+                    console.log(S(data).between('', '.').s )
+                    exec('cp /Users/killer/Desktop/checkerv1.py /Users/killer/Desktop/10_24pm/uploaded_files/'+req.session.user.username+'/'+S(data).between('', '.').s, function(error, stdout, stderr) {});
+                    console.log('copied');
+              });      
+              fs.readFile(__dirname + '/uploaded_files/'+req.session.user.username+'/'+req.session.user.username +'.txt', 'utf8', function (err,data) {
+                  if (err) {
+                    return console.log(err);
+                  }
+                  console.log(data);
+                  var options = {
+                      mode: 'text',
+                      args: [req.session.user.username,S(data).between('', '.').s]
+                  };
+
+                  PythonShell.run('checkerv1.py',options, function(error, stdout, stderr) {
+                       if (error) {
+                        return console.log(error);
+                      }else{
+                        console.log('phani-----------------------------------------------------------');
+                      }
+                  });
+
+                  });             
+              res.sendFile(__dirname + '/public/dashboard_downloadOption.html');
+        	  }
+
+        	setTimeout(function1, 3000);	
+        }
+      });
+    }else{
+      res.redirect('/login');
+    }
+      }else{
+        res.redirect('/admin_dashboard');
+      }
+});
+
+app.post('/download', function(req, res) {
+
+ 
+  console.log("came to post download");
+
+  fs.readFile(__dirname + '/uploaded_files/'+req.session.user.username+'/'+req.session.user.username +'.txt', 'utf8', function (err,data) {
+         if (err) {
+         return console.log(err);
+      }
+      console.log(data);
+      console.log(S(data).between('', '.').s )
+      console.log('copied----');
+      res.download(__dirname+'/uploaded_files/'+req.session.user.username+'/'+S(data).between('', '.').s +'/Report.txt');
+  }); 
+
+});
+
+app.post('/download_2', function(req, res) {
+ 
+  console.log("came to post download");
+  fs.readFile(__dirname + '/uploaded_files/'+req.session.user.username+'/'+req.session.user.username +'.txt', 'utf8', function (err,data) {
+         if (err) {
+         return console.log(err);
+      }
+      console.log(data);
+      console.log(S(data).between('', '.').s )
+      console.log('copied----');
+      res.download(__dirname+'/uploaded_files/'+req.session.user.username+'/'+S(data).between('', '.').s +'/scores.csv');
+  }); 
+
+});
+
+
+app.post('/admin_loggingin',function(req,res){
+
+	var username = req.body.username;
+	Admin.findOne({username: req.body.username},function(err,admin){
+		if(!admin){
+
+			res.sendFile(path.join(__dirname+'/public/admin_login_incorrect.html'));
+		}else{
+			if(req.body.password === admin.password){
+				req.session.admin = admin;
+				res.redirect('/admin_dashboard')
+
+			}else{
+				res.sendFile(path.join(__dirname+'/public/admin_login_incorrect.html'));
+
+			}
+		}
+	});
+});
+
+
+
+
+app.post('/loggingin',function (req,res) {
+  var username = req.body.username;
+
+  User.findOne({username: req.body.username},function (err,user) {
+    if(!user){
+      res.sendFile(path.join(__dirname+'/public/login_incorrect.html'));
+    }else{
+      if(req.body.password === user.password){
+        req.session.user = user;
+        res.redirect('/dashboard');
+      }else{
+        res.sendFile(path.join(__dirname+'/public/login_incorrect.html'));
+      }
+    }
+  });
+});
+
+
+app.post('/signingup',function (req,res) {
+  var username = req.body.username;
+  var password = req.body.password;
+  var firstname = req.body.firstname;
+  var lastname = req.body.lastname;
+  var user = new User();
+
+  user.username = username;
+  user.password = password;
+  user.firstname = firstname;
+  user.lastname = lastname;
+
+  user.save(function (err,savedObject) {
+    if(err){
+      console.log(err);
+      res.sendFile(path.join(__dirname+'/public/signup_incorrect.html'));
+    }else{
+      res.sendFile(path.join(__dirname+'/public/login_new_password.html'));
+    }
+  });
+});
+
+
+
+app.post('/delete_user',function (req,res) {
+  var username = req.body.username;
+  User.findOneAndRemove({username: username},function (err,foundObject) {
+    if(err){
+      console.log(err);
+      res.status(500).send();
+    }if(!foundObject){
+      res.send('not able to remove !! may be no user');
+    }else {
+      res.send('successfully removed');
+    }
+  });
+});
+
+
+
+app.post('/setting_new_password',function (req,res) {
+  var username = req.body.username;
+  var password = req.body.password;
+  var confirm_password = req.body.confirm_password;
+
+  if(password != confirm_password){
+    res.sendFile(path.join(__dirname+'/public/forgot_password_incorrect_username_or_password.html'));
+  }else {
+
+
+    User.findOne({username: username},function (err,foundObject) {
+      if(err){
+        console.log(err);
+        res.status(500).send();
+      }else {
+
+        if(!foundObject){
+          res.sendFile(path.join(__dirname+'/public/forgot_password_incorrect_username.html'));
+        }else {
+          if(req.body.password){
+            foundObject.password = password;
+          }
+
+          foundObject.save(function (err,updateObject) {
+            if(err){
+              console.log(err);
+              return res.status(500).send();
+            }else {
+              res.sendFile(path.join(__dirname+'/public/login_new_password.html')); 	
+            }
+          });
+        }
+
+      }
+    });
+  }
+});
+
+
+app.listen(3000, function (err) {
+  if(err){
+    console.log('port may be in use : bind exception')
+  }else{
+    console.log('Example aapp listening on port 3000!');
+  }
+});
---
+++ 10_24pm/checkerv1.py
@@ -0,0 +1,184 @@
+'''
+A code checker tool for Java, C, CPP. Gives feedback of compile error/runtime error/accepted/wrong/TLE
+'''
+
+import os, filecmp, subprocess, csv, sys, shutil
+
+
+def compile(fileName, lang):
+    if lang == 'c':
+        outFile = fileName[:-2]
+    elif lang == 'cpp':
+        outFile = fileName[:-4]
+    elif lang == 'java':
+        outFile = fileName[:-4] + 'class'
+    #If file with same name exists already, remove it
+    if os.path.isfile(outFile):
+        os.remove(outFile)
+    if lang == 'java':
+        printFile = outFile[-6]
+    else:
+        printFile = fileName
+    #Try to compile the file
+    if os.path.isfile(fileName):
+        try:
+            if lang == 'java':
+                compile_out = subprocess.check_output('javac ' + fileName, stderr=open('temp/'+printFile+'_compile_err.txt', 'w'), shell=True)
+            elif lang == 'c' or lang == 'cpp':
+                compile_out = subprocess.check_output('g++ -o ' + outFile + ' ' + fileName, stderr=open('temp/'+printFile+'_compile_err.txt', 'w'), shell=True)
+        except subprocess.CalledProcessError as e:
+            err = 'Compile error'
+
+        with open('temp/'+printFile+'_compile_err.txt', 'r') as compile_file:
+            compile_err = compile_file.read()
+
+        if os.path.isfile(outFile):
+            return 1, outFile
+        else:
+            return -1, compile_err
+    else:
+        return -2, 'No file named '+fileName+'found in the current directory'
+
+
+def run(fileName, inputFile, timeout, lang):
+    if lang == 'java':
+        cmd = 'java ' + fileName[-6]
+    elif lang == 'c' or lang == 'cpp':
+        cmd = './' + fileName
+
+    if lang == 'java':
+        outFile = fileName[-6]
+    else:
+        outFile = fileName
+    outFile += '_out.txt'
+    outFile = 'temp/' + outFile
+
+    # terminal_cmd = os.system('timeout '+timeout+' '+cmd+' < '+inputFile+' > '+outFile)
+    # if terminal_cmd == 32512:
+    try: 
+        if sys.platform == 'darwin':
+            terminal_cmd = subprocess.check_output('function timeout() { perl -e \'alarm shift; exec @ARGV\' "$@";} ;' + 'timeout '+timeout+' '+cmd+' < '+inputFile+' > '+outFile, stderr = open('temp/'+inputFile+'_runtime_err.txt', 'w'), shell=True)
+        if sys.platform == 'linux' or sys.platform == 'linux2':
+            terminal_cmd = subprocess.check_output('timeout '+timeout+' '+cmd+' < '+inputFile+' > '+outFile, stderr = open('temp/'+inputFile+'_runtime_err.txt', 'w'), shell=True)
+
+    except subprocess.CalledProcessError as e:
+        with open('temp/'+inputFile+'_runtime_err.txt', 'r') as runtime_file:
+            runtime_err = runtime_file.read()
+            if runtime_err.find('Alarm') == -1:
+                if lang == 'java':
+                    os.remove(fileName)
+                elif lang == 'c' or lang == 'cpp':
+                    os.remove(fileName)
+
+                return -3, runtime_err
+
+            else:
+                if lang == 'java':
+                    os.remove(fileName+'.class')
+                elif lang == 'c' or lang == 'cpp':
+                    os.remove(fileName)
+
+                return -4, 'Timeout'
+
+
+    # Remove all extraneous files produced
+    if lang == 'java':
+        os.remove(fileName+'.class')
+    elif lang == 'c' or lang == 'cpp':
+        os.remove(fileName)
+    if terminal_cmd == '':
+        return 1, 'Ran successfully'
+    else:
+        return -2, 'File not found in current directory'
+
+
+def calcScore(correct_output_file, subm_output_file, verbose=False):
+    wrongOutput = []
+    score = 0
+    count = 0
+    with open(correct_output_file, 'r') as correct_file:
+        correct_output = correct_file.readlines()
+    with open('temp/'+subm_output_file, 'r') as subm_file:
+        subm_output = subm_file.readlines()
+    num_testcases = len(correct_output)
+    report = ''
+    for correct_val, subm_val in zip(correct_output, subm_output):
+        # print correct_val, subm_val
+        count += 1
+        if correct_val == subm_val:
+            score += 1
+        else:
+            wrongOutput.append(count)
+            report = report + '\nTest case: ' + str(count) + '\nExpected: ' + str(correct_val) + '\nSubmitted: ' + str(subm_val) 
+            if verbose:
+                print 'Test case:', count
+                print 'Expected:', correct_val
+                print 'Submitted:', subm_val
+                print '\n'
+
+    return (score, num_testcases, report)
+
+def printScore(fileName, testin, testout, timeout, lang):
+    compile_code, compile_prompt = compile(fileName, lang)
+    compiled, ran = False, False
+    if compile_code == 1:
+        compiled = True
+    else:
+        compile_report = compile_prompt
+        return compiled, ran, 'NA', compile_report, 'NA', 'NA'
+    run_code, run_prompt = run(compile_prompt, testin, timeout, lang)
+    if run_code == 1:
+        ran = True
+    elif run_code == -4:
+        return compiled, 'Timeout', 'NA', 'No compile time errors', 'Timeout: Taking too long', 'NA'
+    else:
+        runtime_report = run_prompt
+        return compiled, ran, 'NA', 'No compile time errors', runtime_report, 'NA'
+    score, num_testcases, run_report = calcScore(testout, compile_prompt+'_out.txt', verbose=True)
+    return compiled, ran, score, 'No compile time errors', 'No runtime errors', run_report
+    # print 'score/num_testcases:', str(score)+"/"+str(num_testcases)
+
+if __name__ == '__main__':
+
+    subm_dir = '/Users/killer/Desktop/10_24pm/uploaded_files/'+sys.argv[1]+'/'+sys.argv[2]
+    print subm_dir
+    subm_folder, testin, testout, timeout = subm_dir, 'test.txt', 'out.txt', '2'
+    os.chdir(subm_folder)
+
+    scores = {}
+    if not os.path.isdir('temp'):
+        os.mkdir('temp')
+
+    for subm in os.listdir(subm_folder):
+        lang = ''
+        if subm.endswith('.c'):
+            lang = 'c'
+        elif subm.endswith('.cpp'):
+            lang = 'cpp'
+        elif subm.endswith('.java'):
+            lang = 'java'
+        else:
+            continue
+        compiled, ran, score, compile_report, runtime_report, run_report = printScore(subm, testin, testout, timeout, lang)
+        scores[subm] = map(str, (compiled, ran, score, compile_report, runtime_report, run_report))
+        
+    with open('scores.csv', 'w') as scoreFile:
+        writer = csv.writer(scoreFile)
+        writer.writerow(('FileName', 'Compiled successfully', 'Ran successfully', 'Score'))
+        for k, v in scores.iteritems():
+            writer.writerow([k] + v[:3])
+
+    with open('Report.txt', 'w') as reportFile:
+        for k, v in scores.iteritems():
+            reportFile.write(str(k)+'\n\n')
+            reportFile.write('\nCompilation errors:\n')
+            reportFile.write(str(v[3])+'\n')
+            print v[3], len(v[3]), v[3][0]
+            reportFile.write('\nRun-time errors:\n')
+            reportFile.write(str(v[4])+'\n')
+            reportFile.write('\nWrong output on testcases:\n')
+            reportFile.write(str(v[5])+'\n\n')
+            reportFile.write('****************************************\n')
+    # shutil.rmtree('temp')
+
+
